6883d32dc55d5ba96d480555f9a4863f0c501bda
Miguel Rodrigues <miguel11rodrigues@gmail.com>
2025-07-08 19:00:10 +0100

    Redirects

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.' // coloca na raiz da pasta dist
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
   },
-  base: '/', 
+  base: '/',
   server: {
     host: true
   }
-})
+});
